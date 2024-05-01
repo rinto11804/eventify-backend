4fd521d53448a08fc3843ab5d1f5ad9e0c7ebfd0
rinto11804 <rinto11804@gmail.com>
2024-05-01 07:41:33 +0530

    event schema

---
+++ src/modules/event/event.schema.ts
@@ -0,0 +1,24 @@
+
+import { buildJsonSchemas } from 'fastify-zod'
+import { z } from 'zod'
+
+
+const createEventSchema = z.object({
+  title: z.string().min(4),
+  description: z.string().min(10),
+  date: z.string().date(),
+  from: z.string().time(),
+  to: z.string().time(),
+})
+
+
+export const { schemas: eventSchema, $ref: $eventRef } = buildJsonSchemas(
+  {
+    createEventSchema,
+  },
+  {
+    $id: 'event'
+  }
+)
+
+type CreateEventBody = z.infer<typeof createEventSchema>
